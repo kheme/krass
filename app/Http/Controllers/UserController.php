4c0bc9b9d1a9a5edb3e5cdeb175a8f2da4bf6908
Okiemute Omuta <iamkheme@gmail.com>
2020-08-12 06:06:12 +0100

    Create UserController@store

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * User controller
+ *
+ * PHP version 7
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Okiemute Omuta <iamkheme@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights retained
+ * @link      https://twitter.com/kheme
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Main UserController class
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Okiemute Omuta <iamkheme@gmail.com>
+ * @copyright 2020 Okiemute Omuta. All rights reserved.
+ * @license   All rights retained
+ * @link      https://twitter.com/kheme
+ */
+class UserController
+{
+    /**
+     * Creat a new user
+     *
+     * @param \Illuminate\Http\Request $request HTTP request
+     * 
+     * @author Okiemute Omuta <iamkheme@gmail.com>
+     * 
+     * @return json
+     */
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+            $new_user = User::create([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'password' => $request->password,
+            ]);
+
+            DB::commit();
+            
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'User created and authenticated successfully',
+                    'data'    => [
+                        'id'    => $new_user->id,
+                        'token' => $new_user->createToken('_kheme_')->accessToken
+                    ]
+                ]
+            );
+        } catch (\Exception $exception) {
+            DB::rollBack();
+
+            return response()->json(
+                [
+                    'success' => 'false',
+                    'message' => $exception->getMessage(),
+                ],
+                400
+            );
+        }
+
+        return response()->json(
+            [
+                'success' => 'false',
+                'message' => 'Could not create new user',
+            ],
+            500
+        );
+    }
+}
