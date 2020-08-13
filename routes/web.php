445545e8231a75072570eb27c3db94ad689d4cd8
Okiemute Omuta <iamkheme@gmail.com>
2020-08-13 19:23:57 +0100

    Create secure endpoints

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,19 @@
 |
 */
 
-$router->post('register', 'UserController@store');
+$router->post('register',   'UserController@store');
+$router->post('auth',       'AuthController@store');
+
+$router->delete('auth',     ['middleware' => 'auth:api', 'uses' => 'AuthController@destroy']);
+
+$router->get('banks',       ['middleware' => 'auth:api', 'uses' => 'BankController@index']);
+
+$router->get('transfers',   ['middleware' => 'auth:api', 'uses' => 'TransferController@index']);
+$router->post('transfers',  ['middleware' => 'auth:api', 'uses' => 'TransferController@store']);
+
+$router->get('recipients',  ['middleware' => 'auth:api', 'uses' => 'RecipientController@index']);
+$router->post('recipients', ['middleware' => 'auth:api', 'uses' => 'RecipientController@store']);
+
+$router->delete('recipients/{recipient_code}',  ['middleware' => 'auth:api', 'uses' => 'RecipientController@destroy']);
+
+
