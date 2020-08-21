6a5181a26d1e29fbd3736d334133baf41d42cc52
Okiemute Omuta <iamkheme@gmail.com>
2020-08-21 21:29:34 +0100

    Update users table migration file
    Update database engine, collation and charset

--- database/migrations/2020_08_12_045541_create_users_table.php
+++ database/migrations/2020_08_12_045541_create_users_table.php
@@ -14,6 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine    = 'InnoDB ROW_FORMAT=DYNAMIC';
+            $table->collation = 'utf8_unicode_ci';
+            $table->charset   = 'utf8';
+
             $table->unsignedBigInteger('id', true);
             $table->string('name');
             $table->string('email')->unique();
