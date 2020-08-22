74e7a4da522cf8092d815671241a6373f1140db6
Okiemute Omuta <iamkheme@gmail.com>
2020-08-22 17:07:49 +0100

    Create Controller@sendRequest
    Since we have same header and Paystack secret with each request,
    I decided to create a method for them all

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * Handle POST requests to Paystack
+     *
+     * @param string $url
+     * @param array $request_data
+     *
+     * @author Okiemute Omuta <iamkheme@gmail.com>
+     *
+     * @return array
+     */
+    protected function sendRequest(string $method, string $url, array $request_data = []) : array
+    {
+        $response = Http::withHeaders([ 'Content-Type' => 'application/json' ])
+            ->withToken(env('PAYSTACK_SECRET_KEY'))
+            ->{$method}($url, $request_data);
+        
+        if (! $response->successful()) {
+            throw new Exception($response->json()['message'], $response->status());
+        }
+
+        return $response->json();
+    }
 }
